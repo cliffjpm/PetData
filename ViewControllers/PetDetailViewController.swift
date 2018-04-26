a0651b6d10ec7a6812b57808fd26e4b320586674
Cliff M Anderson <anderson_cliff@mac.com>
2018-04-26 14:52:07 -0400

    Pet Detail View added - able to add pet and cancel
    Pet Detail View added - able to add pet and cancel with name, sex, dob and picture. Need to add ability to edit.
    
    Also, it's a mystery why the outlets are not showing for delegate and datasource on the xCode UI. In fact, in the other project (PuppyVaccines), I only set these in xCode and did not need to add them programmatically.

--- ViewControllers/PetDetailViewController.swift
+++ ViewControllers/PetDetailViewController.swift
@@ -10,23 +10,153 @@ import UIKit
 import CloudKit
 import os.log
 
-class PetDetailViewController: UIViewController, UITextFieldDelegate {
+class PetDetailViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
     //MARK: Properties
-    @IBOutlet weak var petName: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var petNameField: UITextField!
+    @IBOutlet weak var sexPicker: UIPickerView!
+    @IBOutlet weak var birthDateTxt: UITextField!
     @IBOutlet weak var saveButton: UIBarButtonItem!
     
     var pet: Pet?
-    //var pets = [Pet]()
+    var dateFormatter : DateFormatter!
+    let datePicker = UIDatePicker()
     
-    //MARK: Actions
-    /*@IBAction func savePet(_ sender: Any) {
-        let pet = Pet(petName: petName.text!)
-        pet.save()
-    }*/
+    var dobSelected: Date?
+    
+    let sex = ["Sex", "Male", "Female"]
+    var sexSelected: String?
+    
+    //MARK: UI Actions
+    @IBAction func dateFieldTouched(_ sender: Any) {
+     saveButton.isEnabled = false
+    }
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        updateSaveButtonState()
+        
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        // Disable the Save button while editing.
+        saveButton.isEnabled = false
+    }
+    
+    //MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func createDatePicker(){
+        
+        //format date picker
+        datePicker.datePickerMode = .date
+        
+        //toolbar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        //bar button item
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(dateDonePressed))
+        toolbar.setItems([doneButton], animated: false)
+        
+        //connect the input for this field to the toolbar
+        birthDateTxt.inputAccessoryView = toolbar
+        
+        //assign date picker to text field
+        birthDateTxt.inputView = datePicker
+    }
+    
+    @objc func dateDonePressed(){
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .none
+        birthDateTxt.text = dateFormatter.string(from: datePicker.date)
+        dobSelected = datePicker.date
+        self.view.endEditing(true)
+        saveButton.isEnabled = true
+    }
+    
+    
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+    
+    
+        // Hide the keyboard.
+        petNameField.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .photoLibrary
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return sex[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return sex.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        //he sex must not be "Sex"
+        if (sex[row] == "Sex") {
+            sexSelected = nil
+            
+        }
+        else {
+            sexSelected = sex[row]
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        var pickerLabel: UILabel? = (view as? UILabel)
+        if pickerLabel == nil {
+            pickerLabel = UILabel()
+            pickerLabel?.font = UIFont(name: (pickerLabel?.font?.fontName)!, size: 13)
+            pickerLabel?.textAlignment = .center
+        }
+        pickerLabel?.text = sex[row]
+        pickerLabel?.textColor = UIColor.blue
+        
+        return pickerLabel!
+    }
+    
     
     //MARK: Navigation
-    
     // This method lets you configure a view controller before it's presented.
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
@@ -38,35 +168,87 @@ class PetDetailViewController: UIViewController, UITextFieldDelegate {
             return
         }
         
-        //Set the pet that will be saved locally and to iCloud
-        pet = Pet(petName: petName.text!)
+        pet = Pet(petName: petNameField.text!, dob: dobSelected, petSex: sexSelected, photo: photoImageView.image)
         
-        //Save the pet to iCloud if available. This needs to move the unwind area of the TableView
+    }
+    
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+    
+        //os_log("I understand you want to Cancel", log: OSLog.default, type: .debug)
         
-        //pet.save()
-    }
+        // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
+        let isPresentingInAddPetMode = presentingViewController is UINavigationController
+        
+        if isPresentingInAddPetMode {
+            dismiss(animated: true, completion: nil)
+        }
+        else if let owningNavigationController = navigationController{
+            owningNavigationController.popViewController(animated: true)
+            //print("DEBUG as you were not in add mode. This got called and the records got saved. Won't work for a New Dog if addimg meds at the same time")
+        }
+        else {
+            fatalError("The ViewController is not inside a navigation controller.")
+        }}
     
     
-    //MARK: UITextFieldDelegate
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        // Hide the keyboard.
-        textField.resignFirstResponder()
-        return true
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        //PUT YOUR CODE HERE
-    }
-    
-    
+    //MARK: Standard methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        birthDateTxt.font = UIFont(name: (birthDateTxt.font?.fontName)!, size: 13)
+        
+        // Handle the text field’s user input through delegate callbacks.
+        petNameField.delegate = self
+        
+        dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY-MM-dd 'T' hh:mm"
+        
+        self.sexPicker.delegate = self
+        self.sexPicker.dataSource = self
+        
+        createDatePicker()
+        
+        // Set up views if editing an existing Pet.
+        if let pet = pet {
+            petNameField.text = pet.petName
+            photoImageView.image = (pet.photo ?? nil)
+            if pet.dob != nil {
+                datePicker.date = pet.dob!
+                dateDonePressed()
+                
+            } else {
+                dobSelected = nil
+                birthDateTxt.text = "Select Birthday"
+            }
+            
+            sexSelected = (pet.petSex ?? "")
+            switch(pet.petSex ?? "") {
+                
+            case "":
+                sexPicker.selectRow(0, inComponent: 0, animated: true)
+                
+            case "Male":
+                sexPicker.selectRow(1, inComponent: 0, animated: true)
+                
+            case "Female":
+                sexPicker.selectRow(2, inComponent: 0, animated: true)
+                
+            default:
+                fatalError("Unexpected Sex Identifier; \(pet.petSex!)")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: Private Methods
+    private func updateSaveButtonState() {
+        // Disable the Save button if the text field is empty.
+        let text = petNameField.text ?? ""
+        saveButton.isEnabled = !text.isEmpty
     }
     
 
